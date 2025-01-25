7b76d809127f2a8ebcd9ae089074cdba98b3bdba
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2025-01-25 19:46:43 +0330

    changes models.py

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum , Float,ForeignKey
 from app.database import Base
+from sqlalchemy.orm import relationship
 import enum
 
 class UserRole(enum.Enum):
@@ -18,3 +19,31 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     user_role = Column(Enum(UserRole), default=UserRole.CUSTOMER)
+
+class Genre(Base):
+    __tablename__ = "genres"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    books = relationship("Book", back_populates="genre")
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    isbn = Column(String, unique=True, nullable=False)
+    price = Column(Float, nullable=False)
+    genre_id = Column(Integer, ForeignKey("genres.id"), nullable=False)
+    author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    description = Column(String)
+    units = Column(Integer, nullable=False)
+
+    genre = relationship("Genre", back_populates="books")
+    author = relationship("User", back_populates="books")
